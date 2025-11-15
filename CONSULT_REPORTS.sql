c3eb96dbbb93ffc040de55c4bf70e5aa7ff5db79
NataliaMZ-IT <zamperlinin@gmail.com>
2025-11-15 02:25:59 -0300

    Change: adjust parameters and sorting for reports

--- CONSULT_REPORTS.sql
+++ CONSULT_REPORTS.sql
@@ -5,26 +5,25 @@ GO
 SELECT v.DataVenda, v.idVenda, 
   (i.Quantidade * m.ValorVenda) AS ValorTotalItem,
   CONCAT(c.Nome, ' ', c.Sobrenome) AS NomeCliente,
-  c.CPF, m.Nome AS Medicamento, i.Quantidade, m.ValorVenda AS ValorUnidade
+  m.Nome AS Medicamento, i.Quantidade, m.ValorVenda AS ValorUnidade
 FROM Vendas v
 JOIN Clientes c ON v.idCliente = c.idCliente
 JOIN ItensVendas i ON v.idVenda = i.idVenda
 JOIN Medicamentos m ON i.idMedicamento = m.idMedicamento
-WHERE v.DataVenda = '2025-11-07';
+WHERE v.DataVenda BETWEEN '2025-11-07' AND '2025-11-08';
 
 SELECT 
   v.DataVenda, v.idVenda,
   SUM(i.Quantidade * m.ValorVenda) AS Valor,
-  CONCAT(c.Nome, ' ', c.Sobrenome) AS NomeCliente,
-  c.CPF, 
+  CONCAT(c.Nome, ' ', c.Sobrenome) AS NomeCliente, 
   SUM(i.Quantidade) AS QtdItens,
   COUNT(i.idMedicamento) AS ItensDistintos
 FROM Vendas v
 JOIN Clientes c ON v.idCliente = c.idCliente
 JOIN ItensVendas i ON v.idVenda = i.idVenda
 JOIN Medicamentos m ON i.idMedicamento = m.idMedicamento
-WHERE v.DataVenda = '2025-11-07'
-GROUP BY v.idVenda, v.DataVenda, c.Nome, c.Sobrenome, c.CPF;
+WHERE v.DataVenda BETWEEN '2025-11-07' AND '2025-11-08'
+GROUP BY v.idVenda, v.DataVenda, c.Nome, c.Sobrenome;
 GO
 
 -- Report of Medicine Most Sold
@@ -32,9 +31,10 @@ SELECT m.Nome AS Medicamento, c.Nome AS Categoria, i.idVenda,
   i.Quantidade, m.ValorVenda AS Valor,
   (m.ValorVenda * i.Quantidade) AS ValorTotalNaVenda, v.DataVenda
 FROM ItensVendas i
-RIGHT JOIN Medicamentos m ON i.idMedicamento = m.idMedicamento
+LEFT JOIN Medicamentos m ON i.idMedicamento = m.idMedicamento
 LEFT JOIN Vendas v ON i.idVenda = v.idVenda
-LEFT JOIN Categorias c ON m.Categoria = c.id;
+LEFT JOIN Categorias c ON m.Categoria = c.id
+ORDER BY m.Nome ASC;
 
 SELECT m.Nome, c.Nome AS Categoria,
   ISNULL(SUM(i.Quantidade), 0) AS QtdVendida, 
@@ -53,18 +53,20 @@ GO
 SELECT f.RazaoSocial AS Fornecedor, c.idCompra, p.Nome AS PrincipioAtivo, 
   i.Quantidade, i.ValorUnitario, i.ValorTotal, c.DataCompra 
 FROM Compras c
-RIGHT JOIN Fornecedores f ON c.idFornecedor = f.idFornecedor
+LEFT JOIN Fornecedores f ON c.idFornecedor = f.idFornecedor
 LEFT JOIN ItensCompras i ON c.idCompra = i.idCompra
-LEFT JOIN PrincipiosAtivos p ON i.idPrincipio = p.idPrincipio;
+LEFT JOIN PrincipiosAtivos p ON i.idPrincipio = p.idPrincipio
+ORDER BY f.RazaoSocial;
 
 SELECT f.RazaoSocial AS Fornecedor, 
-COUNT(DISTINCT c.idCompra) AS QuantidadeCompras,
-COUNT(i.idCompra) AS ItensComprados,
-ISNULL(SUM(i.Quantidade), 0) AS QtdTotalItens,
-ISNULL(SUM(i.ValorTotal), 0) AS ValorTotalGasto, 
-MAX(c.DataCompra) AS UltimaCompra
+  COUNT(DISTINCT c.idCompra) AS QuantidadeCompras,
+  COUNT(i.idCompra) AS ItensComprados,
+  ISNULL(SUM(i.Quantidade), 0) AS QtdTotalItens,
+  ISNULL(SUM(i.ValorTotal), 0) AS ValorTotalGasto, 
+  MAX(c.DataCompra) AS UltimaCompra
 FROM Compras c
 RIGHT JOIN Fornecedores f ON c.idFornecedor = f.idFornecedor
 LEFT JOIN ItensCompras i ON c.idCompra = i.idCompra
-GROUP BY f.RazaoSocial;
+GROUP BY f.RazaoSocial
+ORDER BY QtdTotalItens DESC;
 GO
